6d058be74507b93aae686c00b4e61ed08b576ff4
zyH <hzy18023@gmail.com>
2025-02-05 22:34:15 +0800

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div w-100vw grid="~ cols-1 rows-1 gap-4">
+    <div text="green" bg-amber-100>
+      hello index
+    </div>
+    <div
+      class="i-twemoji-grinning-face-with-smiling-eyes hover:i-twemoji-face-with-tears-of-joy hover:h-32px hover:w-32px"
+      h-32px w-32px place-self-center
+    />
+  </div>
+</template>
